974ee6fd20dfa20a6a2b8cf8c0c8f233b0d52fe3
Your NamePhilip <philip.adler@hotmail.com>
2023-02-16 09:48:50 -0300

    FIX: Validations needed retification

--- src/middlewares/Validation/product.validation.js
+++ src/middlewares/Validation/product.validation.js
@@ -3,14 +3,13 @@ const { nameValidation } = require('./schema');
 const validateProduct = (req, res, next) => {
   const { name } = req.body;
   const { error } = nameValidation.validate({ name });
-  const { details } = error;
 
-  if (details) {
-    if (details[0].type === 'string.min') {
-      return res.status(422).json({ message: details[0].message });
+  if (error) {
+    if (error.details[0].type === 'string.min') {
+      return res.status(422).json({ message: error.details[0].message });
     }
-    if (details[0].type === 'any.required') {
-      return res.status(400).json({ message: details[0].message });
+    if (error.details[0].type === 'any.required') {
+      return res.status(400).json({ message: error.details[0].message });
     }
   }
 
